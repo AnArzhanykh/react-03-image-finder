91606e6420e9c3fe3d80276c31c9002ca592ee9e
AnArzhanykh <45142693+AnArzhanykh@users.noreply.github.com>
2021-08-24 22:17:40 +0300

    axious

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import "./App.scss";
+import {Component} from 'react';
 import ImageGallery from './components/ImageGallery'
 import Searchbar from './components/Searchbar'
 import Button from './components/Button'
 import Modal from './components/Modal'
 import Loader from './components/Loader'
+import axios from "axios";
 
 
 
 
 
-function App() {
-  return <>
-    <Searchbar />
-    <ImageGallery />
-    <Button />
-    {/* <Modal /> */}
-    <Loader />
-  </>;
+// function App() {
+
+//   const onChangeQuery = query =>{
+//     console.log(query);
+//     const numberPAge = 1;
+//         const key = '21709969-2aaf655592c1caf292dc08cd8';
+//         const url=`https://pixabay.com/api/?q=${query}&page=${numberPAge}&key=${key}&image_type=photo&orientation=horizontal&per_page=12` 
+//         // const pool = axios.get(url).then(response=>response.data).then(data=> this.setState({hits: data.hits})) 
+//         const pool = axios.get(url).then(response=>this.setState({hits: response.data.hits}))
+//   }
+//   return <>
+//     <Searchbar onSubmit={onChangeQuery}/>
+//     <ImageGallery />
+//     <Button />
+//     {/* <Modal /> */}
+//     <Loader />
+//   </>;
   
+// }
+
+// export default App;
+
+
+
+class App extends Component {
+
+  state ={
+      hits: [],
+  }
+
+  onChangeQuery (query) {
+
+    console.log(query);
+    const numberPAge = 1;
+        const key = '21709969-2aaf655592c1caf292dc08cd8';
+        const url=`https://pixabay.com/api/?q=${query}&page=${numberPAge}&key=${key}&image_type=photo&orientation=horizontal&per_page=12` 
+        // const pool = axios.get(url).then(response=>response.data).then(data=> this.setState({hits: data.hits})) 
+        const pool = axios.get(url).then(response=>this.setState({hits: response.data.hits}))
+  }
+
+  render(){
+      return(
+        <>
+          <Searchbar onSubmit={this.onChangeQuery.bind(this)}/>
+          <ImageGallery />
+          <Button />
+          {/* <Modal /> */}
+          <Loader />
+        </>
+      )
+  }
 }
 
 export default App;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -2,29 +2,41 @@ import {Component} from 'react';
 import styles from './Searchbar.module.scss'
 import axios from 'axios'
 
-
+// const AUTH_TOKEN = '21709969-2aaf655592c1caf292dc08cd8';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 class Searchbar extends Component {
 
     state ={
-        searchText: '',
-        numberPAge: 1,
+        query: '',
+        // numberPAge: 1,
+        // hits: [],
     }
-    
+    // componentDidMount(){
+    //     const {query, numberPAge} = this.state;
+    //     const key = '21709969-2aaf655592c1caf292dc08cd8';
+    //     const url=`https://pixabay.com/api/?q=${query}&page=${numberPAge}&key=${key}&image_type=photo&orientation=horizontal&per_page=12` 
+    //     // const pool = axios.get(url).then(response=>response.data).then(data=> this.setState({hits: data.hits})) 
+    //     const pool = axios.get(url).then(response=>this.setState({hits: response.data.hits}))
+    // }
+
     dadawda (e){
-        const {searchText, numberPAge} = this.state;
         e.preventDefault();
-        const key = '21709969-2aaf655592c1caf292dc08cd8';
-        const url=`https://pixabay.com/api/?q=${searchText}&page=${numberPAge}&key=${key}&image_type=photo&orientation=horizontal&per_page=12` 
-        console.log(url);
+        this.props.onSubmit(this.state.query)
+        this.setState({query: ''})
+        // const {query, numberPAge} = this.state;
+        // const key = '21709969-2aaf655592c1caf292dc08cd8';
+        // const url=`https://pixabay.com/api/?q=${query}&page=${numberPAge}&key=${key}&image_type=photo&orientation=horizontal&per_page=12` 
+        // console.log(url);
+
     }
     handlerChanged = (e)=>{
-        this.setState({searchText: e.target.value});
+        this.setState({query: e.currentTarget.value});
     }
     render(){
         return(
             <header className={styles.Searchbar}>
                 <form className={styles.SearchForm}>
-                    <button type="submit" className={styles.SearchForm__button} onClick={this.dadawda.bind(this)}>
+                    <button type="submit" className={styles.SearchForm__button} onClick={()=>this.dadawda}>
                         <span className={styles.SearchForm__button__label}>Search</span>
                     </button>
 
@@ -32,12 +44,13 @@ class Searchbar extends Component {
                         className={styles.SearchForm__input}
                         type="text"
                         autocomplete="off"
-                        value={this.state.searchText}
+                        value={this.state.query}
                         autofocus
                         placeholder="Search images and photos"
                         onChange={this.handlerChanged}
                     />
                 </form>
+
             </header>
         )
     }
