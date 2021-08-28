d3e36c6c26c0423c19485e0bb3bf35457ef95116
AnArzhanykh <45142693+AnArzhanykh@users.noreply.github.com>
2021-08-28 21:01:46 +0300

    7

--- src/App.js
+++ src/App.js
@@ -78,8 +78,9 @@ class App extends Component {
         hits: [...prevState.hits, ...hits],
         currentPage: prevState.currentPage + 1,
       }))
+      this.autoScrollDown()
     }).catch(error=>this.setState({error})).finally(()=>this.setState({isLoading: false}))
-    this.autoScrollDown()
+
 }
 
 
