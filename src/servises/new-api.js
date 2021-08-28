2545d5319fbf44cdc9f8460044a6083147dbb1ab
AnArzhanykh <45142693+AnArzhanykh@users.noreply.github.com>
2021-08-28 20:26:45 +0300

    3

--- src/servises/new-api.js
+++ src/servises/new-api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const AUTH_TOKEN = '21709969-2aaf655592c1caf292dc08cd8';
-axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+const AUTH_TOKEN = `21709969-2aaf655592c1caf292dc08cd8`;
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 
 const fetchImage = ({searchQuery = '', currentPage = 1, pageSize = 12})=>{
-    // const key = '21709969-2aaf655592c1caf292dc08cd8';
-    const url=`https://pixabay.com/api/?q=${searchQuery}&page=${currentPage}&image_type=photo&orientation=horizontal&per_page=${pageSize}` 
+    const url=`https://pixabay.com/api/?q=${searchQuery}&page=${currentPage}&key=${AUTH_TOKEN}&image_type=photo&orientation=horizontal&per_page=${pageSize}` 
     return(
 
         
